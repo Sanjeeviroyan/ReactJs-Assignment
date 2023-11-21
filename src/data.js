d156f5a9b3568cc262de3361ca2e32f1988b1784
Sanjeeviroyan <151127728+Sanjeeviroyan@users.noreply.github.com>
2023-11-21 16:46:22 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const users = [
+    {
+    id: 1,
+    name: 'Sanjeevi',
+    email: 'sanjeevi@example.com',
+    age: 34,
+    },
+    {
+    id: 2,
+    name: 'Akilan',
+    email: 'akilan@example.com',
+    age: 28,
+    },
+    {
+    id: 3,
+    name: 'Ben',
+    email: 'ben@example.com',
+    age: 46,
+    },
+    {
+    id: 4,
+    name: 'chan',
+    email: 'chan@example.com',
+    age: 38,
+    },
+   ];
